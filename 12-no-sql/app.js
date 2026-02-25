44aaa3f0e9ec9c95f18ee08679b54a9d2a19840a
Javenhun733r <andrij.babyuk@gmail.com>
2026-02-25 17:27:43 +0200

    added code for sections 12-16

---
+++ 12-no-sql/app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const User = require('./models/user');
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+	User.findById('699ec23cdc7c43055e47f3b0')
+		.then(user => {
+			req.user = new User(user.name, user.email, user.cart, user._id);
+			next();
+		})
+		.catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+mongoConnect(() => {
+	app.listen(3000);
+});
