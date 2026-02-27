b4c3d794609e639566a8397fd2613a850d201dbe
Javenhun733r <andrij.babyuk@gmail.com>
2026-02-27 17:52:38 +0200

    added code for sections 21-27

---
+++ 27-websockets/backend/app.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+const app = express();
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'images');
+	},
+	filename: function (req, file, cb) {
+		const extension = file.originalname.split('.').pop();
+		cb(null, uuidv4() + '.' + extension);
+	},
+});
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+app.use(bodyParser.json());
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+});
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	const data = error.data;
+	res.status(status).json({ message: message, data: data });
+});
+mongoose
+	.connect(process.env.DB_URL)
+	.then(result => {
+		const server = app.listen(8080);
+		const io = require('./socket').init(server);
+		io.on('connection', socket => {
+			console.log('Client connected');
+		});
+	})
+	.catch(err => console.log(err));
